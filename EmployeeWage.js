23e358f8671a94615b6fff4c60ddc2d144400e44
Yogesh More <iyogesh.more1998@gmail.com>
2022-05-29 14:17:23 +0530

    Added : Ability to calculate wage till given condition.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,30 +1,36 @@
 console.log("Welcome to Employee payroll program");
 
- // UC4 :Calculating Wages for a Month assuming 20 Working Days in a Month
+//UC5 :Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month
 
- const IS_PART_TIME = 1;  
- const IS_FULL_TIME = 2; 
- const PART_TIME_HOURS = 4; 
- const FULL_TIME_HOURS = 8; 
- const WAGE_PER_HOUR = 20; 
- const NUM_OF_WORKING_DAYS = 20;
- function getWorkingHours (empCheck)
+const IS_PART_TIME = 1;  
+const IS_FULL_TIME = 2; 
+const PART_TIME_HOURS = 4; 
+const FULL_TIME_HOURS = 8; 
+const WAGE_PER_HOUR = 20; 
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+
+function getWorkingHours (empCheck)
  {
      switch(empCheck)
 {
     case IS_PART_TIME:
-        return PART_TIME_HOURS;
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-            default:
-                return 0;
+    return PART_TIME_HOURS;
+    case IS_FULL_TIME:
+     return FULL_TIME_HOURS;
+    default:
+     return 0;
 }
  }
-let empHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++)
+let empHours = 0;
+let totalWorkingDays = 0;
+let totalWorkingHours = 0;
+while (totalWorkingDays <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) 
 {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random()*10)%3;
-    empHrs += getWorkingHours(empCheck);
+    totalWorkingHours += getWorkingHours(empCheck);
 }
-let empWage = empHrs * empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs: " + empHrs +" -->"+" Employee Wage: " +empWage)
+let empWage = totalWorkingHours * WAGE_PER_HOUR;
+console.log("Total Days: "+totalWorkingDays +" Total Hours: "+totalWorkingHours+ " Employee wage: "+ empWage);
+
